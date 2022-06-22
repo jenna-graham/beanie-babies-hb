f9db5329bc03654398a55623cb63bf4c654c7a84
jenna-graham <jclayto2@gmail.com>
2022-06-22 14:54:04 -0700

    routing in App.js, page button clicks

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import {
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
 
+
 export default function App() {
   return (
     <Router>
       <div className='App'>
         <Switch>
-          <Route>
-            {/* this home page route should list all the beanies */}
+          <Route exact path="/">
+            <BeaniesPage />
           </Route>
-          <Route> 
+          <Route exact path="/beanies/:id"> 
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+            <BeanieDetail />
           </Route>
         </Switch>
       </div>
