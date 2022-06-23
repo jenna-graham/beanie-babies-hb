5db6289c8c4207101a94bfecee093dc76de8c7d4
jenna-graham <jclayto2@gmail.com>
2022-06-23 12:45:20 -0700

    deploy fix

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -9,7 +9,7 @@ export default function BeanieBaby({ title, image, id }) {
       </div>
     </Link>
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
-    // it should also render the beanie baby's image and show the beanie baby's name
+    // it should also render the baby's image and show the beanie baby's name
   
   );
 }
